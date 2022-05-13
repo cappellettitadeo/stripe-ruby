b39525e5d7fb018c85d87293844aaa050ed64a64
Kamil Pajdzik <kamil@stripe.com>
2022-05-13 15:41:35 -0700

    Codegen for openapi e689865

---
+++ lib/stripe/resources/treasury/transaction_entry.rb
@@ -0,0 +1,12 @@
+# File generated from our OpenAPI spec
+# frozen_string_literal: true
+
+module Stripe
+  module Treasury
+    class TransactionEntry < APIResource
+      extend Stripe::APIOperations::List
+
+      OBJECT_NAME = "treasury.transaction_entry"
+    end
+  end
+end
